9a319d16a910cb888511daff78fbb270266c5fa8
ChandanasreeV <chandanamadhu3148@gmail.com>
2025-07-22 15:21:22 +0530

    [Chanadan]: Ability to delete a person using person name

--- src/AddressBookMain.ts
+++ src/AddressBookMain.ts
@@ -1,34 +1,57 @@
 import { AddressBook } from './model/AddressBook';
-import * as readline from "readline-sync"
+import * as readline from "readline-sync";
 
-class AddressBookMain
-{
-  private addressBook= new AddressBook()
+class AddressBookMain {
+    private addressBook = new AddressBook();
 
-    welcomeToAddressBook():void
-   {
-     console.log("Welcome to the Address Book Program");
-   }
-  
-   run():void
-   {
-    this.welcomeToAddressBook()
-    const personContact=this.addressBook.getContactFromUser()
-    this.addressBook.addAccount(personContact)
-    this.addressBook.getAllContacts()
+    welcomeToAddressBook(): void {
+        console.log("Welcome to the Address Book Program");
+    }
 
-    const nameToEdit=readline.question("Enter first name to edit :")
+    run(): void {
+        this.welcomeToAddressBook();
 
-    this.addressBook.editContact(nameToEdit,{})
+        while (true) {
+            console.log("\n--- MENU ---");
+            console.log("1. Add Contact");
+            console.log("2. View Contacts");
+            console.log("3. Edit Contact by First Name");
+            console.log("4. Delete Contact by First Name");
+            console.log("5. Exit");
 
-    this.addressBook.getAllContacts()
+            const choice = readline.question("Enter your choice (1-5): ");
 
+            switch (choice) {
+                case "1":
+                    const contact = this.addressBook.getContactFromUser();
+                    this.addressBook.addAccount(contact);
+                    console.log("Contact added successfully.");
+                    break;
 
+                case "2":
+                    this.addressBook.getAllContacts();
+                    break;
 
-   
+                case "3":
+                    const nameToEdit = readline.question("Enter first name to edit: ");
+                    this.addressBook.editContact(nameToEdit, {});
+                    break;
 
-   }
-   
+                case "4":
+                    const nameToDelete = readline.question("Enter first name to delete: ");
+                    this.addressBook.deleteContactByName(nameToDelete);
+                    break;
+
+                case "5":
+                    console.log("Exiting program. Goodbye!");
+                    return;
+
+                default:
+                    console.log("Invalid choice. Please select between 1 to 5.");
+            }
+        }
+    }
 }
-const addressApp =new AddressBookMain()
-addressApp.run()
+
+const addressApp = new AddressBookMain();
+addressApp.run();
--- src/model/AddressBook.ts
+++ src/model/AddressBook.ts
@@ -1,71 +1,87 @@
-import * as readline from "readline-sync"
+import * as readline from "readline-sync";
+import { ContactPerson } from "./ContactPerson";
 
-import {ContactPerson} from "./ContactPerson"
-export class AddressBook{
+export class AddressBook {
+    private contacts: ContactPerson[] = [];
 
-    private contacts:ContactPerson[]=[]
-
-    addAccount(contact:ContactPerson):void
-    {
-        this.contacts.push(contact)
-    }
-    
-    getAllContacts(): void 
-    {
-    console.log(" Contact List:");
-    this.contacts.forEach((contact, i) => console.log(`${i + 1}. ${contact.toString()}`));
+    // UC1 & UC2 - Add contact
+    addAccount(contact: ContactPerson): void {
+        this.contacts.push(contact);
     }
 
-    //*UC2-Add-New-Contact-->Adding contact by taking I/P from console
-    getContactFromUser():ContactPerson
-   {
-    const firstName=readline.question("First name: ")
-    const lastName=readline.question("Second name: ")
-    const address=readline.question("Address: ")
-    const city=readline.question("City: ")
-    const state=readline.question("State: ")
-    const zipcode=parseInt(readline.question("Zipcode: "))
-    const phoneNumber=parseInt(readline.question("Phone Number: "))
-    const email=readline.question("Email: ")
-    return new ContactPerson(firstName,lastName,address,city,state,zipcode,phoneNumber,email)
-   }
-
-  //*UC3-edit-contact-->Editing contact by first name
-  editContact(firstName:string,updateDetails:Partial<ContactPerson>):boolean
-  {
-    const contact = this.contacts.find(c => c.firstName === firstName);
-
-    if(contact)
-    {
-       Object.assign(contact,updateDetails)
-       console.log("\nDo you want to edit this contact");
-       const choice=readline.question("Type 'yes' to edit ").toLowerCase()
-    if(choice==='yes')
-    {
-      const newLastName=readline.question("Second name: ")
-      const newAddress=readline.question("Address: ")
-      const newCity=readline.question("City: ")
-      const newState=readline.question("State: ")
-      const newiZpcode=parseInt(readline.question("Zipcode: "))
-      const newPhoneNumber=parseInt(readline.question("Phone Number: "))
-      const newEmail=readline.question("Email: ")
-
-      Object.assign(contact,{
-        lastName:newLastName,
-        address:newAddress,
-        city:newCity,
-        state:newState,
-        zipcode:newiZpcode,
-        phoneNumber:newPhoneNumber,
-        email:newEmail,
-      });
+    // Show all contacts
+    getAllContacts(): void {
+        console.log("\nContact List:");
+        if (this.contacts.length === 0) {
+            console.log("No contacts available.");
+        } else {
+            this.contacts.forEach((contact, i) => console.log(`${i + 1}. ${contact.toString()}`));
+        }
     }
-      console.log("Contact updated succesfully");
-      return true
-    }
-    console.log("Contact not found");
-    return false
-  }
-    
 
+    // UC2 - Get contact input from user
+    getContactFromUser(): ContactPerson {
+        const firstName = readline.question("First name: ");
+        const lastName = readline.question("Second name: ");
+        const address = readline.question("Address: ");
+        const city = readline.question("City: ");
+        const state = readline.question("State: ");
+        const zipcode = parseInt(readline.question("Zipcode: "));
+        const phoneNumber = parseInt(readline.question("Phone Number: "));
+        const email = readline.question("Email: ");
+        return new ContactPerson(firstName, lastName, address, city, state, zipcode, phoneNumber, email);
+    }
+
+    // UC3 - Edit contact by first name
+    editContact(firstName: string, updateDetails: Partial<ContactPerson>): boolean {
+        const contact = this.contacts.find(c => c.firstName === firstName);
+
+        if (contact) {
+            console.log("\nDo you want to edit this contact?");
+            const choice = readline.question("Type 'yes' to edit: ").toLowerCase();
+
+            if (choice === 'yes') {
+                const newLastName = readline.question("Second name: ");
+                const newAddress = readline.question("Address: ");
+                const newCity = readline.question("City: ");
+                const newState = readline.question("State: ");
+                const newZipcode = parseInt(readline.question("Zipcode: "));
+                const newPhoneNumber = parseInt(readline.question("Phone Number: "));
+                const newEmail = readline.question("Email: ");
+
+                Object.assign(contact, {
+                    lastName: newLastName,
+                    address: newAddress,
+                    city: newCity,
+                    state: newState,
+                    zipcode: newZipcode,
+                    phoneNumber: newPhoneNumber,
+                    email: newEmail,
+                });
+
+                console.log("Contact updated successfully.");
+                return true;
+            } else {
+                console.log("Edit cancelled.");
+                return false;
+            }
+        }
+
+        console.log("Contact not found.");
+        return false;
+    }
+
+    // UC4 - Delete contact by first name
+    deleteContactByName(firstName: string): boolean {
+        const index = this.contacts.findIndex(c => c.firstName === firstName);
+
+        if (index !== -1) {
+            const deleted = this.contacts.splice(index, 1);
+            console.log(`\nContact '${deleted[0].firstName} ${deleted[0].lastName}' deleted successfully.`);
+            return true;
+        } else {
+            console.log("\nContact not found. Deletion failed.");
+            return false;
+        }
+    }
 }
